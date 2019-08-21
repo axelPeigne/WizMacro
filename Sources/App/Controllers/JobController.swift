fd424a5f421bfa7220755803cc6b849f41ed6977
Axel Peigné <axel@lamarckise.fr>
2019-08-21 18:10:54 +0200

    Reverted changes

--- Sources/App/Controllers/JobController.swift
+++ Sources/App/Controllers/JobController.swift
@@ -38,34 +38,34 @@ final class JobController {
         return try req.parameters.next(Job.self).flatMap { job in
             return job.script
                 .get(on: req)
-                .flatMap({ script -> EventLoopFuture<Job> in
-                    return try req.content.decode(Car.self).flatMap { car in
-                        _ = car.save(on: req)
-                        job.status = .succeded
-                        return job.save(on: req)
-                    }
-                })
-//                .do({ script in
-//                    if script.identifier == .vrs08 {
-//                        guard let data = req.http.body.data else {
-//                            job.status = .empty
-//                            return
-//                        }
-//                        job.jsonResult = data
-//                        guard let cars = try? JSONDecoder().decode([Car].self, from: data) else {
-//                            if let logger = try? req.make(Logger.self) {
-//                                logger.info("wrong data")
-//                            }
-//                            job.status = .errored
-//                            return
-//                        }
-//                        for car in cars {
-//                            _ = car.save(on: req)
-//                        }
+//                .flatMap({ script -> EventLoopFuture<Job> in
+//                    return try req.content.decode(Car.self).flatMap { car in
+//                        _ = car.save(on: req)
 //                        job.status = .succeded
-//                        _ = job.save(on: req)
+//                        return job.save(on: req)
 //                    }
-//            })
+//                })
+                .do({ script in
+                    if script.identifier == .vrs08 {
+                        guard let data = req.http.body.data else {
+                            job.status = .empty
+                            return
+                        }
+                        job.jsonResult = data
+                        guard let cars = try? JSONDecoder().decode([Car].self, from: data) else {
+                            if let logger = try? req.make(Logger.self) {
+                                logger.info("wrong data")
+                            }
+                            job.status = .errored
+                            return
+                        }
+                        for car in cars {
+                            _ = car.save(on: req)
+                        }
+                        job.status = .succeded
+                        _ = job.save(on: req)
+                    }
+            })
         }.transform(to: .ok)
     }
     
